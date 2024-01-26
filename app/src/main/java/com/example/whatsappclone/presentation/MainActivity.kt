e2de0bcc9235cec3bb084e1af3daecae0962c141
HarshAProgrammer <App.Lavishly@Gmail.com>
2024-01-26 18:04:41 +0530

    Binding enabled

---
+++ app/src/main/java/com/example/whatsappclone/presentation/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.whatsappclone.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.whatsappclone.R
+import com.example.whatsappclone.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
